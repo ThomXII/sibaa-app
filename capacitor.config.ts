e5d37e1fea653fffc45add03da1db8b9cbcb2402
Thomas <thomaswilliamstw0@gmail.com>
2022-11-22 16:37:13 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'sibaa',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
